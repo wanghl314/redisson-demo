520b169e25e7ab49646876839427394483fdfebe
王洪亮 <w505957125@aliyun.com>
2023-09-21 11:44:23 +0800

    1. upgrade to redisson 3.23.5 2. remove deprecated api invoke

--- src/main/java/com/whl/redission/TestRedission.java
+++ src/main/java/com/whl/redission/TestRedission.java
@@ -1,6 +1,6 @@
 package com.whl.redission;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 import org.redisson.Redisson;
 import org.redisson.api.RBucket;
@@ -14,20 +14,20 @@ public class TestRedission {
         Config config = new Config();
         config.useSingleServer().setAddress("redis://192.168.1.243:6379").setDatabase(0).setPassword("em7_redis");
         RedissonClient redisson = Redisson.create(config);
-        RBucket bucket = redisson.getBucket("test");
+        RBucket<Object> bucket = redisson.getBucket("test");
         Object oldValue = bucket.get();
 
         if (oldValue != null) {
             System.out.println("oldValue=" + oldValue);
         } else {
-            bucket.set("value", 20, TimeUnit.SECONDS);
+            bucket.set("value", Duration.ofSeconds(20));
             System.out.println(redisson.getBucket("test").get());
         }
         RLock lock = redisson.getLock("test-lock");
 
         if (lock.tryLock()) {
             Thread.sleep(10000L);
-            redisson.getBucket("other").set("other-value", 20, TimeUnit.SECONDS);
+            redisson.getBucket("other").set("other-value", Duration.ofSeconds(20));
         }
         lock.unlock();
         redisson.shutdown();
